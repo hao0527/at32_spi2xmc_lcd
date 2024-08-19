4454301d259ae9cbc814cb498eb8026d77e491cb
hao <33110820@qq.com>
2024-08-19 08:34:09 +0800

    更新：缩进转空格

--- mysrc/drv/st7796s.c
+++ mysrc/drv/st7796s.c
@@ -21,9 +21,9 @@
 /*The LCD needs a bunch of command/argument values to be initialized. They are stored in this struct. */
 typedef struct
 {
-	uint8_t cmd;
-	uint8_t data[16];
-	uint8_t databytes; //No of data in data; bit 7 = delay after set; 0xFF = end of cmds.
+    uint8_t cmd;
+    uint8_t data[16];
+    uint8_t databytes; //No of data in data; bit 7 = delay after set; 0xFF = end of cmds.
 } lcd_init_cmd_t;
 
 /**********************
@@ -53,110 +53,110 @@ static inline uint16_t lcd_rd_data(void);
  **********************/
 void st7796s_init(void)
 {
-	lcd_init_cmd_t init_cmds[] = {
-		{0xCF, {0x00, 0x83, 0X30}, 3},
-		{0xED, {0x64, 0x03, 0X12, 0X81}, 4},
-		{0xE8, {0x85, 0x01, 0x79}, 3},
-		{0xCB, {0x39, 0x2C, 0x00, 0x34, 0x02}, 5},
-		{0xF7, {0x20}, 1},
-		{0xEA, {0x00, 0x00}, 2},
-		{0xC0, {0x26}, 1},		 /*Power control*/
-		{0xC1, {0x11}, 1},		 /*Power control */
-		{0xC5, {0x35, 0x3E}, 2}, /*VCOM control*/
-		{0xC7, {0xBE}, 1},		 /*VCOM control*/
-		{0x36, {0x28}, 1},		 /*Memory Access Control*/
-		{0x3A, {0x55}, 1},		 /*Pixel Format Set*/
-		{0xB1, {0x00, 0x1B}, 2},
-		{0xF2, {0x08}, 1},
-		{0x26, {0x01}, 1},
-		{0xE0, {0x1F, 0x1A, 0x18, 0x0A, 0x0F, 0x06, 0x45, 0X87, 0x32, 0x0A, 0x07, 0x02, 0x07, 0x05, 0x00}, 15},
-		{0XE1, {0x00, 0x25, 0x27, 0x05, 0x10, 0x09, 0x3A, 0x78, 0x4D, 0x05, 0x18, 0x0D, 0x38, 0x3A, 0x1F}, 15},
-		{0x2A, {0x00, 0x00, 0x00, 0xEF}, 4},
-		{0x2B, {0x00, 0x00, 0x01, 0x3f}, 4},
-		{0x2C, {0}, 0},
-		{0xB7, {0x07}, 1},
-		{0xB6, {0x0A, 0x82, 0x27, 0x00}, 4},
-		{0x11, {0}, 0x80},
-		{0x29, {0}, 0x80},
-		{0, {0}, 0xff},
-	};
+    lcd_init_cmd_t init_cmds[] = {
+        {0xCF, {0x00, 0x83, 0X30}, 3},
+        {0xED, {0x64, 0x03, 0X12, 0X81}, 4},
+        {0xE8, {0x85, 0x01, 0x79}, 3},
+        {0xCB, {0x39, 0x2C, 0x00, 0x34, 0x02}, 5},
+        {0xF7, {0x20}, 1},
+        {0xEA, {0x00, 0x00}, 2},
+        {0xC0, {0x26}, 1},		 /*Power control*/
+        {0xC1, {0x11}, 1},		 /*Power control */
+        {0xC5, {0x35, 0x3E}, 2}, /*VCOM control*/
+        {0xC7, {0xBE}, 1},		 /*VCOM control*/
+        {0x36, {0x28}, 1},		 /*Memory Access Control*/
+        {0x3A, {0x55}, 1},		 /*Pixel Format Set*/
+        {0xB1, {0x00, 0x1B}, 2},
+        {0xF2, {0x08}, 1},
+        {0x26, {0x01}, 1},
+        {0xE0, {0x1F, 0x1A, 0x18, 0x0A, 0x0F, 0x06, 0x45, 0X87, 0x32, 0x0A, 0x07, 0x02, 0x07, 0x05, 0x00}, 15},
+        {0XE1, {0x00, 0x25, 0x27, 0x05, 0x10, 0x09, 0x3A, 0x78, 0x4D, 0x05, 0x18, 0x0D, 0x38, 0x3A, 0x1F}, 15},
+        {0x2A, {0x00, 0x00, 0x00, 0xEF}, 4},
+        {0x2B, {0x00, 0x00, 0x01, 0x3f}, 4},
+        {0x2C, {0}, 0},
+        {0xB7, {0x07}, 1},
+        {0xB6, {0x0A, 0x82, 0x27, 0x00}, 4},
+        {0x11, {0}, 0x80},
+        {0x29, {0}, 0x80},
+        {0, {0}, 0xff},
+    };
 
-	//Reset the display
-	gpio_bits_write(ST7796S_RST, FALSE);
-	delay_ms(100);
-	gpio_bits_write(ST7796S_RST, TRUE);
-	delay_ms(100);
+    //Reset the display
+    gpio_bits_write(ST7796S_RST, FALSE);
+    delay_ms(100);
+    gpio_bits_write(ST7796S_RST, TRUE);
+    delay_ms(100);
 
     uint8_t id[4];
-	st7796s_send_cmd(0xD3);
+    st7796s_send_cmd(0xD3);
     st7796s_recv_data(id, 4);
-	if (id[2] != 0x77 || id[3] != 0x96)
-	{
-		while(1);	// id check error
-	}
+    if (id[2] != 0x77 || id[3] != 0x96)
+    {
+        while(1);	// id check error
+    }
 
-	//Send all the commands
-	uint16_t cmd = 0;
-	while (init_cmds[cmd].databytes != 0xff)
-	{
-		st7796s_send_cmd(init_cmds[cmd].cmd);
-		st7796s_send_data(init_cmds[cmd].data, init_cmds[cmd].databytes & 0x1F);
-		if (init_cmds[cmd].databytes & 0x80)
-		{
-			delay_ms(100);
-		}
-		cmd++;
-	}
+    //Send all the commands
+    uint16_t cmd = 0;
+    while (init_cmds[cmd].databytes != 0xff)
+    {
+        st7796s_send_cmd(init_cmds[cmd].cmd);
+        st7796s_send_data(init_cmds[cmd].data, init_cmds[cmd].databytes & 0x1F);
+        if (init_cmds[cmd].databytes & 0x80)
+        {
+            delay_ms(100);
+        }
+        cmd++;
+    }
 
-	st7796s_set_orientation(ST7796S_DISPLAY_ORIENTATION);
+    st7796s_set_orientation(ST7796S_DISPLAY_ORIENTATION);
 
 #if ST7796S_INVERT_COLORS == 1
-	st7796s_send_cmd(0x21);
+    st7796s_send_cmd(0x21);
 #else
-	st7796s_send_cmd(0x20);
+    st7796s_send_cmd(0x20);
 #endif
 }
 
 void st7796s_flush(const lcd_area_t *area, lcd_color_t *color_map)
 {
-	uint8_t data[4];
+    uint8_t data[4];
 
-	/*Column addresses*/
-	st7796s_send_cmd(0x2A);
-	data[0] = (area->x1 >> 8) & 0xFF;
-	data[1] = area->x1 & 0xFF;
-	data[2] = (area->x2 >> 8) & 0xFF;
-	data[3] = area->x2 & 0xFF;
-	st7796s_send_data(data, 4);
+    /*Column addresses*/
+    st7796s_send_cmd(0x2A);
+    data[0] = (area->x1 >> 8) & 0xFF;
+    data[1] = area->x1 & 0xFF;
+    data[2] = (area->x2 >> 8) & 0xFF;
+    data[3] = area->x2 & 0xFF;
+    st7796s_send_data(data, 4);
 
-	/*Page addresses*/
-	st7796s_send_cmd(0x2B);
-	data[0] = (area->y1 >> 8) & 0xFF;
-	data[1] = area->y1 & 0xFF;
-	data[2] = (area->y2 >> 8) & 0xFF;
-	data[3] = area->y2 & 0xFF;
-	st7796s_send_data(data, 4);
+    /*Page addresses*/
+    st7796s_send_cmd(0x2B);
+    data[0] = (area->y1 >> 8) & 0xFF;
+    data[1] = area->y1 & 0xFF;
+    data[2] = (area->y2 >> 8) & 0xFF;
+    data[3] = area->y2 & 0xFF;
+    st7796s_send_data(data, 4);
 
-	/*Memory write*/
-	st7796s_send_cmd(0x2C);
+    /*Memory write*/
+    st7796s_send_cmd(0x2C);
 
-	uint32_t size = (area->x2 - area->x1 + 1) * (area->y2 - area->y1 + 1);
+    uint32_t size = (area->x2 - area->x1 + 1) * (area->y2 - area->y1 + 1);
 
-	st7796s_send_color((void *)color_map, size);
+    st7796s_send_color((void *)color_map, size);
 }
 
 void st7796s_sleep_in()
 {
-	uint8_t data[] = {0x08};
-	st7796s_send_cmd(0x10);
-	st7796s_send_data(data, 1);
+    uint8_t data[] = {0x08};
+    st7796s_send_cmd(0x10);
+    st7796s_send_data(data, 1);
 }
 
 void st7796s_sleep_out()
 {
-	uint8_t data[] = {0x08};
-	st7796s_send_cmd(0x11);
-	st7796s_send_data(data, 1);
+    uint8_t data[] = {0x08};
+    st7796s_send_cmd(0x11);
+    st7796s_send_data(data, 1);
 }
 
 /**********************
@@ -192,17 +192,17 @@ static void st7796s_send_color(uint16_t *data, uint32_t length)
 static void st7796s_set_orientation(uint8_t orientation)
 {
 #if defined CONFIG_LV_PREDEFINED_DISPLAY_M5STACK
-	uint8_t data[] = {0x68, 0x68, 0x08, 0x08};
+    uint8_t data[] = {0x68, 0x68, 0x08, 0x08};
 #elif defined(CONFIG_LV_PREDEFINED_DISPLAY_WROVER4)
-	uint8_t data[] = {0x4C, 0x88, 0x28, 0xE8};
+    uint8_t data[] = {0x4C, 0x88, 0x28, 0xE8};
 #elif defined(CONFIG_LV_PREDEFINED_DISPLAY_WT32_SC01)
-	uint8_t data[] = {0x48, 0x88, 0x28, 0xE8};
+    uint8_t data[] = {0x48, 0x88, 0x28, 0xE8};
 #elif defined(CONFIG_LV_PREDEFINED_DISPLAY_NONE)
-	uint8_t data[] = {0x48, 0x88, 0x28, 0xE8};
+    uint8_t data[] = {0x48, 0x88, 0x28, 0xE8};
 #endif
 
-	lcd_wr_command(0x36);
-	st7796s_send_data((void *)&data[orientation], 1);
+    lcd_wr_command(0x36);
+    st7796s_send_data((void *)&data[orientation], 1);
 }
 
 //////////////////////////////////////xmc///////////////////////////////////////
